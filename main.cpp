6f5e2ca79083e5b61578c7681a5168024f2ab501
ThatRandomSpbuStudent <sokhanivan@yandex.ru>
2024-05-14 10:26:54 +0300

    Removed possibility to allocate negative memory

--- main.cpp
+++ main.cpp
@@ -5,11 +5,16 @@
 volatile char global_volatile_reader;
 
 int main() {
-  unsigned long long kill_memory;
+  long long kill_memory;
   std::cout << "enter memory top kill in gb: \n";
   std::cin >> kill_memory;
-
-  char *killer = new char[kill_memory * 1024 * 1024 * 1024];
+  char *killer;
+  if (kill_memory > 0) {
+    killer = new char[kill_memory * 1024 * 1024 * 1024];
+  } else {
+    std::cout << "entered incorrect data";
+    return 0;
+  }
 
   char work_mode;
   std::cout << "enter work mode: \n";
@@ -19,7 +24,7 @@ int main() {
   int step;
   std::cout << "enter step in pages:\n";
   std::cin >> step;
-  if (work_mode == 'w' && kill_memory > 0 && step > 0) {
+  if (work_mode == 'w' && step > 0) {
     for (int i = 0; i < (kill_memory * 1024 * 1024 / 4); i++) {
       killer[i * 4096 + 1] = 'g';
       if (i != 0 && i % step == 0) {
@@ -27,7 +32,7 @@ int main() {
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
       }
     }
-  } else if (work_mode == 'r' && kill_memory > 0 && step > 0) {
+  } else if (work_mode == 'r' && step > 0) {
     for (int i = 0; i < (kill_memory * 1024 * 1024 / 4); i++) {
       global_volatile_reader = killer[i * 4096 + 1];
       if (i != 0 && i % step == 0) {
