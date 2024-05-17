a56409abab42a80813576a642a3bd9118e142094
ThatRandomSpbuStudent <sokhanivan@yandex.ru>
2024-05-17 20:28:55 +0300

    Added comments on testing on Linux and Windows

--- main.cpp
+++ main.cpp
@@ -4,9 +4,17 @@
 
 volatile char global_volatile_reader;
 
+/*
+The program worked as intented on Linux in writing mode until it ran out of
+memory and after that was killed by system. However of reading mode it did not
+work at all on Linux.
+On windows the program was killed by system after taking about 2 gb of RAM both
+on read and on write mode.
+*/
+
 int main() {
   long long kill_memory;
-  std::cout << "enter memory top kill in gb: \n";
+  std::cout << "enter memory to kill in gb: \n";
   std::cin >> kill_memory;
   char *killer;
   if (kill_memory > 0) {
@@ -25,7 +33,7 @@ int main() {
   std::cout << "enter step in pages:\n";
   std::cin >> step;
   if (work_mode == 'w' && step > 0) {
-    for (int i = 0; i < (kill_memory * 1024 * 1024 / 4); i++) {
+    for (long i = 0; i < (kill_memory * 1024 * 1024 / 4); i++) {
       killer[i * 4096 + 1] = 'g';
       if (i != 0 && i % step == 0) {
         std::cout << i << "/" << kill_memory * 1024 * 1024 / 4 << "\n";
@@ -33,7 +41,7 @@ int main() {
       }
     }
   } else if (work_mode == 'r' && step > 0) {
-    for (int i = 0; i < (kill_memory * 1024 * 1024 / 4); i++) {
+    for (long i = 0; i < (kill_memory * 1024 * 1024 / 4); i++) {
       global_volatile_reader = killer[i * 4096 + 1];
       if (i != 0 && i % step == 0) {
         std::cout << i << "/" << kill_memory * 1024 * 1024 / 4 << "\n";
@@ -43,5 +51,6 @@ int main() {
   } else {
     std::cout << "entered incorrect data\n";
   }
+
   return 0;
 }
